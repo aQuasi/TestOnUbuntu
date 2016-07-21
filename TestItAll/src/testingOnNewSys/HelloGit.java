6e0d106d871d8c9ddc57b00c8b6ca7213fcc64ad
kay <drickzee@gmail.com>
2016-07-21 05:21:50 -0400

    2nd Test

--- TestItAll/src/testingOnNewSys/HelloGit.java
+++ TestItAll/src/testingOnNewSys/HelloGit.java
@@ -1,11 +1,30 @@
 package testingOnNewSys;
 
+import java.util.Scanner;
+
 public class HelloGit {
 
 	public static void main(String[] args) {
 		
-		String greetings = "Hello, Git!";
-		System.out.println(greetings);
+		//Simple print statement (Testing with Git usage)
+		String greeting = "Hello, Git!";
+		System.out.println(greeting);
+		
+		int num = 5;
+		int myNum;
+		
+		Scanner scan = new Scanner(System.in);
+		myNum = scan.nextInt();
+		
+		System.out.println("Enter a number: ");
+		
+		if (myNum == num) {
+			System.out.println("Cool, " + myNum + " matches!");
+		}
+		else {
+			System.out.println("Wrong!");
+		}
+		scan.close();
 
 	}
 
