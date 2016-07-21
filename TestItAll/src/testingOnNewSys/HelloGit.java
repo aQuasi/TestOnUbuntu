0038a779730a221fc745bb2f13b350fb9a9b257d
kay <drickzee@gmail.com>
2016-07-21 05:04:58 -0400

    First commit

---
+++ TestItAll/src/testingOnNewSys/HelloGit.java
@@ -0,0 +1,12 @@
+package testingOnNewSys;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		
+		String greetings = "Hello, Git!";
+		System.out.println(greetings);
+
+	}
+
+}
